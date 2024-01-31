8732d7c60f6707975e8be1e2a8d94ecf7bf68a87
Aiyumi3 <rsomeonei@gmail.com>
2024-01-31 12:03:09 +0200

    commit ~~~

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import 'uikit/dist/css/uikit.min.css'; //  CSS
+import './index.css';
 
 UIkit.use(Icons); // icons activation
 
